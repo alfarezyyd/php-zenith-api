ef6534a5433e650e9ea27939494586bdb5ed479f
alfarezyyd <adityaalfarezyd@gmail.com>
2024-04-29 18:56:51 +0700

    feat: Store & product schema

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ExpeditionCity extends Model
+class Store extends Model
 {
     use HasFactory;
-    
 }
---
+++ database/migrations/2024_04_29_114901_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug', length: 60)->nullable(false);
+            $table->string('name', length: 50)->nullable(false);
+            $table->enum('condition', ['NEW', 'SECOND'])->default('NEW')->nullable(false);
+            $table->text('description')->nullable(false);
+            $table->unsignedInteger('price')->nullable(false);
+            $table->unsignedSmallInteger('minimum_order')->nullable(false);;
+            $table->enum('status', ['ACTIVE', 'NON_ACTIVE'])->default('ACTIVE')->nullable(false);
+            $table->unsignedMediumInteger('stock')->nullable(false);
+            $table->string('sku', length: 50)->nullable(false);
+            $table->unsignedSmallInteger('weight')->nullable(false);
+            $table->unsignedSmallInteger('width')->nullable(false);
+            $table->unsignedSmallInteger('height')->nullable(false);
+            $table->unsignedBigInteger('store_id')->nullable(false);
+            $table->foreign('store_id')->references('id')->on('stores');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
