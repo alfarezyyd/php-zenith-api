4f0284c207e896c106cce64656ad4747a341aaf5
alfarezyyd <adityaalfarezyd@gmail.com>
2024-04-29 22:48:13 +0700

    feat: initialize API for address

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'label',
+        'street',
+        'neighbourhood_number',
+        'hamlet_number',
+        'village',
+        'urban_village',
+        'sub_district',
+        'expedition_city_id',
+        'expedition_province_id',
+        'postal_code',
+        'note',
+        'receiver_name',
+        'telephone'
+    ];
 }
