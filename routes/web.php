a90cb6787b283135653c6f27ca8a2f589db16d0a
alfarezyyd <adityaalfarezyd@gmail.com>
2024-05-04 00:00:56 +0700

    refactor: delete unnecessary code

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-  use App\Http\Controllers\AddressController;
   use Illuminate\Foundation\Application;
   use Illuminate\Support\Facades\Route;
   use Inertia\Inertia;
@@ -19,7 +18,6 @@
     config('jetstream.auth_session'),
     'verified',
   ])->group(function () {
-    Route::resource('addresses', AddressController::class);
     Route::get('/dashboard', function () {
       return Inertia::render('Dashboard');
     })->name('dashboard');
