fb63f1dc524b03ce444791499edc2dbab5f3a172
alfarezyyd <adityaalfarezyd@gmail.com>
2024-05-04 11:26:00 +0700

    feat: add helper

--- app/Helpers/CommonHelper.php
+++ app/Helpers/CommonHelper.php
@@ -12,5 +12,21 @@
         throw new \HttpResponseException("Error occurred when to write", 400);
       }
     }
+
+    public function slugifyString(string $initializeString): string
+    {
+      // Trim whitespace dari awal dan akhir string
+      $slugifiedString = trim($initializeString);
+
+      // Konversi ke huruf kecil (lowercase)
+      $slugifiedString = strtolower($slugifiedString);
+
+      // Hapus karakter selain huruf kecil (a-z), angka (0-9), spasi, dan dash (-)
+      $slugifiedString = preg_replace('/[^a-z0-9\s-]/', '', $slugifiedString);
+
+      // Ganti satu atau lebih spasi atau dash dengan satu dash tunggal
+      // Kembalikan hasil normalisasi string
+      return preg_replace('/[\s-]+/', '-', $slugifiedString);
+    }
   }
 
