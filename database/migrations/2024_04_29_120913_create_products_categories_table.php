2364889e8556fe9b8bc9dd62aa5e8a8b80a2899f
alfarezyyd <adityaalfarezyd@gmail.com>
2024-05-04 10:15:38 +0700

    refactor: add primary & delete composite primary key

--- database/migrations/2024_04_29_120913_create_products_categories_table.php
+++ database/migrations/2024_04_29_120913_create_products_categories_table.php
@@ -1,22 +1,22 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+  use Illuminate\Database\Migrations\Migration;
+  use Illuminate\Database\Schema\Blueprint;
+  use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+  return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('products_categories', function (Blueprint $table) {
-            $table->unsignedBigInteger('product_id')->nullable(false);
-            $table->unsignedBigInteger('category_id')->nullable(false);
-            $table->primary(['product_id', 'category_id']);
-            $table->timestamps();
-        });
+      Schema::create('products_categories', function (Blueprint $table) {
+        $table->id();
+        $table->unsignedBigInteger('product_id')->nullable(false);
+        $table->unsignedBigInteger('category_id')->nullable(false);
+        $table->unique(['product_id', 'category_id']);
+        $table->timestamps();
+      });
     }
 
     /**
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products_categories');
+      Schema::dropIfExists('products_categories');
     }
-};
+  };
