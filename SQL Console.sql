2dea9829d920a51b4f9aa15abc5f6facdca1a8f9
alfarezyyd <adityaalfarezyd@gmail.com>
2024-05-08 13:24:45 +0700

    feat: some command

--- SQL Console.sql
+++ SQL Console.sql
@@ -1,3 +1,8 @@
 DROP DATABASE IF EXISTS php_zenith_api;
 CREATE DATABASE php_zenith_api;
 USE php_zenith_api;
+SHOW TABLES;
+SELECT * FROM social_accounts;
+SELECT * FROM users;
+SELECT * FROM expedition_cities;
+SELECT * FROM addresses;
