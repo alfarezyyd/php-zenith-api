3070795cd25d0dfe05f6d9f1986cfde44c9e0b26
alfarezyyd <adityaalfarezyd@gmail.com>
2024-05-09 08:25:34 +0700

    feat: make relationship

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,7 +4,9 @@
 
   use Illuminate\Database\Eloquent\Factories\HasFactory;
   use Illuminate\Database\Eloquent\Model;
+  use Illuminate\Database\Eloquent\Relations\BelongsTo;
   use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+  use Illuminate\Database\Eloquent\Relations\HasOne;
 
   class Cart extends Model
   {
@@ -22,5 +24,9 @@
     public function products(): BelongsToMany
     {
       return $this->belongsToMany(Product::class, 'product_carts', 'cart_id', 'product_id');
+    }
+
+    public function user(): HasOne{
+      return $this->hasOne(User::class, 'user_id', 'id');
     }
   }
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@
   // use Illuminate\Contracts\Auth\MustVerifyEmail;
   use Illuminate\Database\Eloquent\Factories\HasFactory;
   use Illuminate\Database\Eloquent\Relations\HasMany;
+  use Illuminate\Database\Eloquent\Relations\HasOne;
   use Illuminate\Foundation\Auth\User as Authenticatable;
   use Illuminate\Notifications\Notifiable;
   use Laravel\Sanctum\HasApiTokens;
@@ -53,5 +54,10 @@
     public function socialAccounts(): HasMany
     {
       return $this->hasMany(SocialAccount::class);
+    }
+
+    public function cart(): HasOne
+    {
+      return $this->hasOne(Cart::class, 'user_id', 'id');
     }
   }
