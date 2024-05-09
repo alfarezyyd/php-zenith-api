0719f2ede2287cd10e23344c71eb956cb70b73ca
alfarezyyd <adityaalfarezyd@gmail.com>
2024-05-09 08:27:52 +0700

    feat: make a cart when user registration

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -3,10 +3,14 @@
   namespace App\Http\Controllers\Auth;
 
   use App\Http\Controllers\Controller;
+  use App\Models\Cart;
   use App\Models\SocialAccount;
   use App\Models\User;
+  use App\Payloads\WebResponsePayload;
   use Exception;
+  use Illuminate\Http\Exceptions\HttpResponseException;
   use Illuminate\Http\JsonResponse;
+  use Illuminate\Support\Facades\DB;
   use Laravel\Socialite\Facades\Socialite;
   use Symfony\Component\HttpFoundation\RedirectResponse;
 
@@ -20,52 +24,64 @@
     public function handleProvideCallback($provider): JsonResponse|\Illuminate\Http\RedirectResponse
     {
       try {
-        $user = Socialite::driver($provider)->stateless()->user();
+        $userModel = Socialite::driver($provider)->stateless()->user();
       } catch (Exception $e) {
         return redirect()->back();
       }
       // find or create user and send params user get from socialite and provider
-      $authUser = $this->findOrCreateUser($user, $provider);
+      $authUser = $this->findOrCreateUser($userModel, $provider);
 
       $token = $authUser->createToken('token_name')->plainTextToken;
       return response()->json(['token' => $token]);
     }
 
+    /**
+     * @throws HttpResponseException
+     */
     public function findOrCreateUser($socialUser, $provider)
     {
-      // Get Social Account
-      $socialAccount = SocialAccount::where('provider_id', $socialUser->getId())
-        ->where('provider_name', $provider)
-        ->first();
+      try {
+        DB::beginTransaction();
+        // Get Social Account
+        $socialAccount = SocialAccount::where('provider_id', $socialUser->getId())
+          ->where('provider_name', $provider)
+          ->first();
 
-      // Jika sudah ada
-      if ($socialAccount) {
-        // return user
-        return $socialAccount->user;
+        // Jika sudah ada
+        if ($socialAccount) {
+          // return user
+          return $socialAccount->user;
 
-        // Jika belum ada
-      } else {
+          // Jika belum ada
+        } else {
 
-        // User berdasarkan email
-        $user = User::where('email', $socialUser->getEmail())->first();
+          // User berdasarkan email
+          $userModel = User::where('email', $socialUser->getEmail())->first();
 
-        // Jika Tidak ada user
-        if (!$user) {
-          // Create user baru
-          $user = User::create([
-            'name' => $socialUser->getName(),
-            'email' => $socialUser->getEmail()
+          // Jika Tidak ada user
+          if (!$userModel) {
+            // Create user baru
+            $userModel = User::create([
+              'name' => $socialUser->getName(),
+              'email' => $socialUser->getEmail()
+            ]);
+            $cartModel = new Cart(['user_id' => $userModel['id']]);
+            $cartModel->save();
+          }
+          // Buat Social Account baru
+          $userModel->socialAccounts()->create([
+            'provider_id' => $socialUser->getId(),
+            'provider_name' => $provider
           ]);
         }
-
-        // Buat Social Account baru
-        $user->socialAccounts()->create([
-          'provider_id' => $socialUser->getId(),
-          'provider_name' => $provider
-        ]);
-
+        DB::commit();
         // return user
-        return $user;
+        return $userModel;
+      } catch (Exception) {
+        DB::rollBack();
+        throw new HttpResponseException(response()->json(
+          new WebResponsePayload("Something went wrong when trying to create an account.", 500),
+        ));
       }
     }
   }
